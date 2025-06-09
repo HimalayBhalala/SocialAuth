239b70c6623845fd21396b29f91052f61d650ea8
HimalayBhalala <bhimalaybhalala96@gmail.com>
2025-06-09 15:12:56 +0530

    Modify main urls.py

--- social_authentication/urls.py
+++ social_authentication/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView
 from authentication.views import DashboardTemplateView
 
+# App URl
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
